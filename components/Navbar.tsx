d6040a2d7abc3a072af2af698b79dd522a4498ee
Carlito Pedida Jr <carlped417@gmail.com>
2025-05-24 20:12:03 -0500

    Intialized Navigation bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import NavItems from "./NavItems";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex item-center gap-2.5 cursor-pointer">
+          <img src="/images/logo.svg" alt="log" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <p>Sign In</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
