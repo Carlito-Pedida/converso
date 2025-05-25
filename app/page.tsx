f8d04334d49ed8da27047e8bb76f87eb13a9dd06
Carlito Pedida Jr <carlped417@gmail.com>
2025-05-24 22:02:00 -0500

    Companion Homepage build

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,51 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/Cta";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xk underline"> Welcome to my SaaS App</h1>
-      <Button>Let get started!</Button>
-    </div>
+    <main>
+      <h1>Popular Companion</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="124"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Intergrals"
+          subject="math"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="125"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
