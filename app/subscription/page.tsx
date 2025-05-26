d6aac9383bc6317df10dfcb97e25867976ff3872
Carlito Pedida Jr <carlped417@gmail.com>
2025-05-25 19:17:02 -0500

    Setup Clerk Auth, Subcriptions, Build Database, Tables, Attributes. Created server actions, dabatabase connections.

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,14 @@
+import { PricingTable } from "@clerk/nextjs";
 import React from "react";
 
 const Subscription = () => {
-  return <div>Subscription</div>;
+  return (
+    <main>
+      <article>
+        <PricingTable />
+      </article>
+    </main>
+  );
 };
 
 export default Subscription;
