f85a9b6b5f9e41f63cffb1763149c981df39de6d
Node-Prophet <85714933+Carlito-Pedida@users.noreply.github.com>
2025-05-26 23:52:20 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -56,9 +56,9 @@ export const configureAssistant = (voice: string, style: string) => {
       ]
     },
 
-    // @ts-expect-error
+
     clientMessages: [],
-    // @ts-expect-error
+ 
     serverMessages: []
   };
   return vapiAssistant;
